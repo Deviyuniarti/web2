7179f73313f5f7877f9ff66a3046ff6d35ea734f
Devi <114374528+Deviyuniarti@users.noreply.github.com>
2023-03-14 15:03:46 +0700

    Add files via upload

---
+++ pertemuan4/function.php
@@ -0,0 +1,12 @@
+<?php
+    require "library.php";
+   
+    sayaHello ("Devi");
+    echo "<br>";
+
+    echo hitungPersegi(25);
+
+    echo "<br>";
+    
+    echo "ini function.php";
+?>
---
+++ pertemuan4/library.php
@@ -0,0 +1,12 @@
+<?php
+    function sayaHello($nama)
+    {
+        echo " Hai,nama saya $nama !";
+    }
+
+    function hitungPersegi($sisi)
+    {
+        return $sisi * $sisi;
+    }
+    
+?>
